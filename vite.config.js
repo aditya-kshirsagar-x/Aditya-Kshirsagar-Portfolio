7e13563ba670f1baeda9bbd9bae5619dce8ff9e0
adityakshirsagar762060-source <adityakshirsagar762060@gmail.com>
2026-09-02 20:54:37 +0530

    Add complete portfolio website

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/aditya-portfolio/', // <--- यह लाइन जोड़ना अनिवार्य है!
+  base: '/aditya-portfolio/',
 })
